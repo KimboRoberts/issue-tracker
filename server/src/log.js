d9ef1d9307c627ba656450260bf5f420b1c095d3
Kim Roberts <kim.roberts@live.co.uk>
2023-04-13 16:45:40 +0100

    Updated logging

--- server/src/log.js
+++ server/src/log.js
@@ -4,7 +4,7 @@ const { splat, combine, timestamp, printf } = winston.format;
 
 // meta param is ensured by splat()
 const customFormat = printf(({ timestamp, level, message, meta }) => {
-    return `${timestamp} ${level.toUpperCase()}\t${message} ${meta ? 'meta: ' + JSON.stringify(meta) : ''}`;
+    return `${timestamp} ${level.toUpperCase()} :: ${message} ${meta ? 'meta: ' + JSON.stringify(meta) : ''}`;
 });
 
 const combinedFormat = combine(timestamp(), splat(), customFormat);
