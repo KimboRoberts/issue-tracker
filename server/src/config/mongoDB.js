7e9600f1475bcf30caea6cd4d9c0d665c5a11e96
Kim Roberts <kim.roberts@live.co.uk>
2023-04-13 17:32:26 +0100

    Added test case for register endpoint

--- server/src/config/mongoDB.js
+++
@@ -1,8 +0,0 @@
-
-clusterUri = uri = 'mongodb+srv://kimroberts:bf52m5AZYkdbIt44@issuetracker.lg2s5it.mongodb.net/?retryWrites=true&w=majority';
-dbName = "IssueTracker"
-
-module.exports = {
-    clusterUri,
-    dbName
-};
